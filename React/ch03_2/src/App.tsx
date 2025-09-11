0afe7853946415268c303d4117a7cf697caae202
Qlellow <changyeon0702@gmail.com>
2025-09-11 12:26:24 +0900

    add

---
+++ React/ch03_2/src/App.tsx
@@ -0,0 +1,15 @@
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
+
+function App() {
+  return (
+    <div className="App">
+      <Tailwindcss />
+      <Color />
+      <TextsTest />
+    </div>
+  )
+}
+
+export default App
---
+++ React/ch03_2/src/data/index.ts
@@ -0,0 +1,4 @@
+export * from './util'
+export * from './chance'
+export * from './data'
+export * from './image'
