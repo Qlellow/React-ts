96e9a5b92f15b705a8a2369ce146b3ef06bcef2b
Qlellow <changyeon0702@gmail.com>
2025-09-18 12:17:23 +0900

    OverlayTest Update

--- React/ch03_3/src/pages/OverlayTest.tsx
+++ React/ch03_3/src/pages/OverlayTest.tsx
@@ -1,10 +1,17 @@
-import { Title } from '../components'
+import { Title, Div, Icon, Overlay } from '../components'
 
-export default function CopyMe() {
+export default function OverlayTest() {
   return (
     <section className="mt-4">
-      <Title>Copy Me</Title>
-      <div className="mt-4"></div>
+      <Title>OverlayTest</Title>
+      <Overlay opacityClass="bg-black/50">
+        <Div className="relative flex items-center justify-center p-8 bg-white h-1/2">
+          <Div className="absolute" right="1rem" top="1rem">
+            <Icon name="close" className="text-gray-500" />
+          </Div>
+          <p className="text-5xl text-black">Modal Dialog Box</p>
+        </Div>
+      </Overlay>
     </section>
   )
 }
