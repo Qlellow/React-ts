4447d6ca34244ce5a8ec8015c0aa5b25222ea411
Qlellow <changyeon0702@gmail.com>
2025-09-18 11:53:29 +0900

    DisplayNoneTest Update

--- React/ch03_3/src/pages/DisplayNoneTest.tsx
+++ React/ch03_3/src/pages/DisplayNoneTest.tsx
@@ -4,7 +4,11 @@ export default function CopyMe() {
   return (
     <section className="mt-4">
       <Title>Copy Me</Title>
-      <div className="mt-4"></div>
+      <div className="mt-4">
+        <p className="visible">Visibility: visible</p>
+        <p className="invisible">Visibility: invisible</p>
+        <p className="hidden">display: none</p>
+      </div>
     </section>
   )
 }
