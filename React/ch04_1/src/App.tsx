c3b440fd44801b056d0b8ce826de0010edc05b11
Qlellow <changyeon0702@gmail.com>
2025-10-16 10:54:26 +0900

    ch04 Clock Created

--- React/ch04_1/src/App.tsx
+++ React/ch04_1/src/App.tsx
@@ -1,23 +1,18 @@
-import DirectionTest from './pages/DirectionTest'
-import WrapTest from './pages/WrapTest'
-import MinMaxTest from './pages/MinMaxTest'
-import JustifyCenterTest from './pages/JustifyCenterTest'
-import AlignTest from './pages/AlignTest'
-import UserContainer from './pages/UserContainer'
-import CardContainer from './pages/CardContainer'
+import { useEffect, useState } from 'react'
+
+import Clock from './pages/Clock'
 
 function App() {
-  return (
-    <div className="App">
-      <CardContainer />
-      <UserContainer />
-      <AlignTest />
-      <JustifyCenterTest />
-      <MinMaxTest />
-      <WrapTest />
-      <DirectionTest />
-    </div>
-  )
+  const [today, setToday] = useState(new Date())
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setToday(new Date())
+    }, 1000)
+    return () => clearInterval(id)
+  }, []) // 의존성 리스트를 빈 배열로 주어 Mount 때 한 번 만 실행되도록 함
+
+  return <Clock today={today} />
 }
 
 export default App
