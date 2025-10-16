bd677482a2df1229485a8de95d983be94b20823b
Qlellow <changyeon0702@gmail.com>
2025-10-16 12:18:45 +0900

    Completed HighOrderCallback.tsx

--- React/ch04_2/src/pages/HighOrderCallback.tsx
+++ React/ch04_2/src/pages/HighOrderCallback.tsx
@@ -1,10 +1,25 @@
+import { useMemo, useCallback } from 'react'
 import { Title } from '../components'
+import * as D from '../data'
 
 export default function CopyMe() {
+  const onClick = useCallback((name: string) => () => alert(`${name} clicked`), [])
+  const buttons = useMemo(
+    () =>
+      D.makeArray(3)
+        .map(D.randomName)
+        .map((name, index) => (
+          <button key={index} onClick={onClick(name)} className="btn btn-primary btn-wide btn-xs">
+            {name}
+          </button>
+        )),
+    [onClick]
+  )
+
   return (
     <section className="mt-4">
-      <Title>Copy Me</Title>
-      <div className="mt-4"></div>
+      <Title>HighOrderCallback</Title>
+      <div className="flex mt-4 justify-evenly">{buttons}</div>
     </section>
   )
 }
