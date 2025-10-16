24fef6fe56f60e043e76f7317bd36a46585afca8
Qlellow <changyeon0702@gmail.com>
2025-10-16 11:25:01 +0900

    Compeleted Custom hooks

--- React/ch04_1/src/App.tsx
+++ React/ch04_1/src/App.tsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useClock } from './hooks'
 
 import Clock from './pages/Clock'
 
 function App() {
-  const [today, setToday] = useState(new Date())
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      setToday(new Date())
-    }, 1000)
-    return () => clearInterval(id)
-  }, []) // 의존성 리스트를 빈 배열로 주어 Mount 때 한 번 만 실행되도록 함
-
+  const today = useClock()
   return <Clock today={today} />
 }
 
