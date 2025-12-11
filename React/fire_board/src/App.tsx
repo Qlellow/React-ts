5aa0b7d6b0e38f3154e2e9715e7a9abde38e9094
Qlellow <changyeon0702@gmail.com>
2025-12-11 10:13:53 +0900

    feature: App.tsx 수정 및 Login.tsx 제작

--- React/fire_board/src/App.tsx
+++ React/fire_board/src/App.tsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import { auth } from './firebase'; // 로컬 설정파일에서 auth 가져오기
+import { onAuthStateChanged, type User } from 'firebase/auth';
+
 import './App.css';
 
 function App() {
-  return <></>;
+  const [user, setUser] = useState<User | null>(null); // 사용자 상태
+  const [loading, setLoading] = useState<boolean>(true); // 로딩 상태
+
+  useEffect(() => {
+    // onAuthStateChanged: auth 상태 구독, 상태가 바뀌면 호출된다.(로그인, 로그아웃)
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser); // 사용자가 있으면 user 객체, 없으면 null
+      setLoading(false); // 로딩 완료
+    });
+    // unmount될 때 데이터 감지를 멈춤
+    return () => unsubscribe();
+  });
+
+  if (loading) {
+    return <div>Loading...</div>; // 로딩 중
+  }
+
+  return (
+    <div className="App">
+      {!user ? <div>로그인 컴포넌트</div> : <div>게시글 목록 컴포넌트</div>}
+    </div>
+  );
 }
 
 export default App;
